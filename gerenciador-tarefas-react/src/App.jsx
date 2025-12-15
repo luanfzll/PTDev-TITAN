9741f52dbbfca1130180da9b5ef8c6019ddd9d4a
Luan Leal <luanfellizolaleal@gmail.com>
2025-12-15 17:50:42 -0300

    fix: corrigir case sensitivity do import tasks

--- gerenciador-tarefas-react/src/App.jsx
+++ gerenciador-tarefas-react/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
-import Tasks from "./components/tasks";
+import Tasks from "./components/Tasks";
 import { v4 } from "uuid";
 import Title from "./components/Title";
 
